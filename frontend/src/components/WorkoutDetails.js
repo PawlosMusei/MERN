e340908b74e602b6f45e93c897769ff12373b3bd
Pawlos <9020097@student.zadkine.nl>
2024-04-02 13:20:19 +0200

    update

---
+++ frontend/src/components/WorkoutDetails.js
@@ -0,0 +1,44 @@
+import { formatDistance } from "date-fns/formatDistance";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+
+//date fns
+import fromatDistanceToNow, {
+  formatDistanceToNow,
+} from "date-fns/formatDistanceToNow";
+
+const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+
+  const handleClick = async () => {
+    const reponse = await fetch("/api/workouts/" + workout._id, {
+      method: "DELETE",
+    });
+    const json = await reponse.json();
+
+    if (reponse.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
+
+  return (
+    <div className="workout-details">
+      <h4>{workout.title}</h4>
+      <p>
+        <strong>Load (kg): </strong>
+        {workout.load}
+      </p>
+      <p>
+        <strong>Number of reps: </strong>
+        {workout.reps}
+      </p>
+      <p>
+        {formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}
+      </p>
+      <span className="material-symbols-outlined" onClick={handleClick}>
+        delete
+      </span>
+    </div>
+  );
+};
+
+export default WorkoutDetails;
